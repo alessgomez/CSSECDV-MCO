1877fe8bdebeb4a28067f2e846f7e6756a4034ee
Alessandra Gomez <94123165+alessgomez@users.noreply.github.com>
2024-06-10 18:27:06 +0800

    Added backend format validation of account details

--- controllers/registration_controller.js
+++ controllers/registration_controller.js
@@ -9,22 +9,26 @@ async function registerAccount(connection, newAccount) {
                 reject(error);
             } else {
                 if (results.length === 0) {            
-                    const config = JSON.parse(fs.readFileSync('config.json'));
-                    error, newAccount.pw = await bcrypt.hash(newAccount.pw, config.saltRounds);
+                    if (validateDetails(newAccount)) {
+                        const config = JSON.parse(fs.readFileSync('config.json'));
+                        error, newAccount.pw = await bcrypt.hash(newAccount.pw, config.saltRounds);
 
-                    if (error) {
-                        reject(error);
+                        if (error) {
+                            reject(error);
+                        } else {
+                            const sql = 'INSERT INTO accounts (firstName, lastName, email, password, phoneNumber, role, dateCreated) VALUES (?, ?, ?, ?, ?, ?, ?)';
+                            const values = [newAccount.first, newAccount.last, newAccount.email, newAccount.pw, newAccount.number, 'USER', new Date()];
+
+                            connection.query(sql, values, async (error, results) => {
+                                if (error) {
+                                    reject(error);
+                                } else {
+                                    resolve(results); // Account successfully registered
+                                }
+                            });
+                        }
                     } else {
-                        const sql = 'INSERT INTO accounts (firstName, lastName, email, password, phoneNumber, role, dateCreated) VALUES (?, ?, ?, ?, ?, ?, ?)';
-                        const values = [newAccount.first, newAccount.last, newAccount.email, newAccount.pw, newAccount.number, 'USER', new Date()];
-
-                        connection.query(sql, values, async (error, results) => {
-                            if (error) {
-                                reject(error);
-                            } else {
-                                resolve(results); // Account successfully registered
-                            }
-                        });
+                        resolve(null); // Invalid account details
                     }
                 } else {
                     resolve(null); // Account with the given email already exists
@@ -32,6 +36,23 @@ async function registerAccount(connection, newAccount) {
             }
         })
     });
+}
+
+function validateDetails(newAccount) {
+    // TODO: Backend format validation for name (?)
+
+    const emailRegex = /^(([_-][A-Za-z0-9]+)|[A-Za-z0-9]+)([_.-][A-Za-z0-9]+)*@[A-Za-z0-9]+(-[A-Za-z0-9]+)*(\.[A-Za-z0-9]+(-[A-Za-z0-9]+)*)*(\.[A-Za-z]{2,})$/
+    const emailValid = emailRegex.test(newAccount.email) && newAccount.email.substr(0, newAccount.email.indexOf('@')).length <= 64 && newAccount.email.substr(newAccount.email.indexOf('@')).length <= 255
+
+    const phoneNumberRegex = /^(09|\+639)\d{9}$/;
+    const phoneNumberValid = phoneNumberRegex.test(newAccount.number);
+    
+    const passwordRegex = /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)(?=.*[^a-zA-Z0-9\s]).{8,64}$/;
+    const passwordValid = passwordRegex.test(newAccount.pw);
+
+    // TODO: Backend format validation for profile pic (?)
+
+    return emailValid && phoneNumberValid && passwordValid; // TODO: Add validation for name and profile pic once done
 }
 
 const registration_controller = {
@@ -55,14 +76,13 @@ const registration_controller = {
             const account = await registerAccount(connection, newAccount);
 
             if (account === null) {
-                req.flash('error_msg', 'Invalid email.');
+                req.flash('error_msg', 'Invalid details.');
                 return res.redirect('/register');
             } else {
                 req.flash('success_msg', 'Account successfully registered. You may log in.');
                 return res.redirect('/login');
             }
         } catch (error) {
-            console.error('Error during account registration:', error);
             req.flash('error_msg', 'An error occurred during registration. Please try again.');
             return res.redirect('/login');
         }
