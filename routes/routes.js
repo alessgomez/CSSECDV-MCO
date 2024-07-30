adb520b9f1c0672db275eb1634b28c4826699d7a
hannahrfong <84816042+hannahrfong@users.noreply.github.com>
2024-07-31 00:25:12 +0800

    added logic for email verification

--- routes/routes.js
+++ routes/routes.js
@@ -46,7 +46,7 @@ app.get('/getBagTotal', general_controller.isPrivate, verifyRole('USER'), bag_co
 app.get('/confirmation/:id', general_controller.isPrivate, verifyRole('USER'), bag_controller.getBag, confirmation_controller.getConfirmation);
 app.get('/orderhistory', general_controller.isPrivate, verifyRole('USER'), bag_controller.getBag, order_history_controller.getOrderHistory);
 app.get('/orderhistory/:index', general_controller.isPrivate, verifyRole('USER'), bag_controller.getBag, order_history_controller.getOrderDetails);
-
+app.get('/verifyEmail/:email', general_controller.isPublic, registration_controller.getVerifyEmail);
 
 // POSTs
 app.post('/verifyAccount', login_controller.postVerifyAccount);
