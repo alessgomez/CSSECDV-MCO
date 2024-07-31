3fa9279cccd135bf32a2c244866256a66a27528a
ibz-kahil <ibrahim_kahil@dlsu.edu.ph>
2024-08-01 01:30:50 +0800

    set samesite back to none

--- index.js
+++ index.js
@@ -39,7 +39,7 @@ app.use(session({
         secure: true, 
         maxAge: 1000 * 60 * 15, 
         httpOnly: true,
-        sameSite: 'lax',
+        sameSite: 'none',
         domain: 'cssecdv-mco.onrender.com',
         path: '/'
     }
