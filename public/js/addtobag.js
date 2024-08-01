44a2d1c4b0d905f98982f4310fbee2ca85be0c01
hannahrfong <84816042+hannahrfong@users.noreply.github.com>
2024-08-01 21:50:42 +0800

    removed addon code

--- public/js/addtobag.js
+++ public/js/addtobag.js
@@ -15,56 +15,13 @@ $(document).ready(function(){
       count = parseInt($("#quantity").text());
       count++;
       $("#quantity").text(count);
-    
-  
-      var countChecked =  $(".addOnOption:checked").length;
-  
-      var checkboxValues = $('.addOnOption:checked').map(function() {
-        return $(this).next("label").text();
-      }).get();
-  
-      for (var i = 0; i < countChecked; i++)
-      {
-        checkboxValues[i] = $.trim(checkboxValues[i]);
-      }
-  
-      var addOnsPrices = [];
-  
+
       var itemQuantity = $("#quantity").text();
       itemQuantity = parseFloat(itemQuantity);
        
-      
       var itemPrice = $("#price").text();
       itemPrice = itemPrice.substring(1);
       itemPrice = parseFloat(itemPrice);
-  
-      if (checkboxValues.length > 0)
-      {
-          for (var i = 0; i < checkboxValues.length; i++)
-          {
-            var name = checkboxValues[i];
-            $.get('/getAddOn', {name: name}, function(result)  {
-              addOnsPrices.push(result.price);
-  
-              var sum = 0;
-              for (var j = 0; j < addOnsPrices.length; j++)
-              {
-                sum = sum + addOnsPrices[j];
-              } 
-              var totalPrice = itemQuantity * (itemPrice + sum);
-  
-              $("#addtobag").text("Add to Bag - ₱" + totalPrice);
-  
-  
-            }); 
-          }
-      }
-      else
-      {
-        var totalPrice = itemQuantity * itemPrice;
-  
-        $("#addtobag").text("Add to Bag - ₱" + totalPrice);
-      }
   
       var totalPrice = itemQuantity * itemPrice;
   
@@ -78,19 +35,6 @@ $(document).ready(function(){
         count--;
         $("#quantity").text(count);
         
-        var countChecked =  $(".addOnOption:checked").length;
-  
-        var checkboxValues = $('.addOnOption:checked').map(function() {
-          return $(this).next("label").text();
-        }).get();
-  
-        for (var i = 0; i < countChecked; i++)
-        {
-          checkboxValues[i] = $.trim(checkboxValues[i]);
-        }
-  
-        var addOnsPrices = [];
-  
         var itemQuantity = $("#quantity").text();
         itemQuantity = parseFloat(itemQuantity);
         
@@ -99,93 +43,11 @@ $(document).ready(function(){
         itemPrice = itemPrice.substring(1);
         itemPrice = parseFloat(itemPrice);
   
-        if (checkboxValues.length > 0)
-        {
-            for (var i = 0; i < checkboxValues.length; i++)
-            {
-              var name = checkboxValues[i];
-              $.get('/getAddOn', {name: name}, function(result)  {
-                addOnsPrices.push(result.price);
-  
-                var sum = 0;
-                for (var j = 0; j < addOnsPrices.length; j++)
-                {
-                  sum = sum + addOnsPrices[j];
-                } 
-                var totalPrice = itemQuantity * (itemPrice + sum);
-  
-                $("#addtobag").text("Add to Bag - ₱" + totalPrice);
-                
-  
-              }); 
-            }
-        }
-        else
-        {
-          var totalPrice = itemQuantity * itemPrice;
-  
-          $("#addtobag").text("Add to Bag - ₱" + totalPrice);
-        }
-  
         var totalPrice = itemQuantity * itemPrice;
   
         $("#addtobag").text("Add to Bag - ₱" + totalPrice);
       }
         
-    });
-  
-    $(".addOnOption").change(function()  {
-  
-      var countChecked =  $(".addOnOption:checked").length;
-      
-  
-      var checkboxValues = $('.addOnOption:checked').map(function() {
-        return $(this).next("label").text();
-      }).get();
-  
-      for (var i = 0; i < countChecked; i++)
-      {
-        checkboxValues[i] = $.trim(checkboxValues[i]);
-      }
-  
-      var addOnsPrices = [];
-  
-      var itemQuantity = $("#quantity").text();
-      itemQuantity = parseFloat(itemQuantity);
-       
-      
-      var itemPrice = $("#price").text();
-      itemPrice = itemPrice.substring(1);
-      itemPrice = parseFloat(itemPrice);
-  
-      if (checkboxValues.length > 0)
-      {
-          for (var i = 0; i < checkboxValues.length; i++)
-          {
-            var name = checkboxValues[i];
-            $.get('/getAddOn', {name: name}, function(result)  {
-              addOnsPrices.push(result.price);
-  
-              var sum = 0;
-              for (var j = 0; j < addOnsPrices.length; j++)
-              {
-                sum = sum + addOnsPrices[j];
-              } 
-              var totalPrice = itemQuantity * (itemPrice + sum);
-  
-              $("#addtobag").text("Add to Bag - ₱" + totalPrice);
-  
-  
-            }); 
-          }
-      }
-      else
-      {
-        var totalPrice = itemQuantity * itemPrice;
-  
-        $("#addtobag").text("Add to Bag - ₱" + totalPrice);
-      }
-   
     });
   
     $("#addtobag").click(function(){
